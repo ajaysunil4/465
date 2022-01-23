8a9b940f4a9dceea17d9d07f69aad0c50dc42c76
ajaysunil4 <ajaysunil84@gmail.com>
2022-01-23 11:41:57 +0530

    commit

--- E-learning/mongodb1.js
+++
@@ -1,8 +0,0 @@
-var MongoClient=require('mongodb').MongoClient;
-var url="mongodb://localhost:27017/YourUdemy";
-MongoClient.connect(url,{useNewUrlParser:true,useUnifiedTopology:true},
-function(err,db){
-if(err)throw err;
-console.log("Database Created");
-db.close();
-});
